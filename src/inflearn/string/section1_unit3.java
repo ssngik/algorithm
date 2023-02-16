ae6e6eb3896338cc5ee924dafa60336abb2b6a60
김상익 <ksik518@naver.com>
2023-02-16 17:22:23 +0900

    section1_unit3_문장 속 단어

---
+++ src/inflearn/string/section1_unit3.java
@@ -0,0 +1,25 @@
+// 3. 문장 속 단어
+
+package inflearn.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class section1_unit3 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = br.readLine();
+
+        String[] s = input.split(" ");
+
+        String answer = " ";
+
+        for(String x : s){
+
+
+        }
+    }
+}
