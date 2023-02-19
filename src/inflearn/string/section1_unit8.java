837bdbc99fb599216318c024588ec796de3a3090
김상익 <ksik518@naver.com>
2023-02-20 00:09:25 +0900

    section1_unit8_유효한 팰린드롬

---
+++ src/inflearn/string/section1_unit8.java
@@ -0,0 +1,27 @@
+package inflearn.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class section1_unit8 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = br.readLine();
+        String answer="YES";
+
+
+        input = input.toLowerCase().replaceAll("[^a-z]", "");
+
+        String tmp = new StringBuilder(input).reverse().toString();
+
+        if(!input.equals(tmp)) answer="NO";
+
+        System.out.println(answer);
+
+
+
+    }
+}
