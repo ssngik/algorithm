61ce3d69faa3017616f48bed2b19cf1b20a61420
김상익 <ksik518@naver.com>
2023-02-21 01:03:44 +0900

    section1_unit9_숫자만 추출

--- src/inflearn/string/section1_unit9.java
+++ src/inflearn/string/section1_unit9.java
@@ -17,11 +17,11 @@ public class section1_unit9 {
             if(!Character.isAlphabetic(input.charAt(i))) answer.append(input.charAt(i));
 
 
+
         System.out.println(Integer.parseInt(answer.toString()));
 
         // 자바 for loop String 관련 검색 공부하기
         // https://choichumji.tistory.com/135 -> 관련 링크
-        // 해당 파일 commit
 
     }
 }
