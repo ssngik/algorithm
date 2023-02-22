60746fad8b5bd316f4045029efc08b27ab694003
김상익 <ksik518@naver.com>
2023-02-22 22:43:00 +0900

    section2_unit1_큰 수 출력하기

--- src/inflearn/array/section2_unit1.java
+++ src/inflearn/array/section2_unit1.java
@@ -1,4 +1,28 @@
 package inflearn.array;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class section2_unit1 {
+    public static void main(String[] args){
+
+        Scanner kb = new Scanner(System.in);
+
+        int n=kb.nextInt();
+
+        int[] arr=new int[n];
+
+        for(int i=0; i<n; i++) arr[i]=kb.nextInt();
+
+        ArrayList<Integer> answer = new ArrayList<>();
+
+        answer.add(arr[0]);
+
+        for(int i=1; i<n; i++){
+            if(arr[i]>arr[i-1]) answer.add(arr[i]);
+        }
+
+        for(int x : answer) System.out.print(x+ " ");
+
+    }
 }
