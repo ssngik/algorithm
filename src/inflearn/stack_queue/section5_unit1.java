f240d558a5134149810a1043c89d62628f38f30a
김상익 <ksik518@naver.com>
2023-03-09 20:55:57 +0900

    section5_unit1_올바른 괄호

--- src/inflearn/stack_queue/section5_unit1.java
+++ src/inflearn/stack_queue/section5_unit1.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 
-public class section4_unit1 {
+public class section5_unit1 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
