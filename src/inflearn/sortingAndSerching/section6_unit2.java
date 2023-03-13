29634d63b90586ed35042fc4c371ad75fb01911a
김상익 <ksik518@naver.com>
2023-03-13 21:24:10 +0900

    section6_unit2_버블 정렬

---
+++ src/inflearn/sortingAndSerching/section6_unit2.java
@@ -0,0 +1,26 @@
+package inflearn.sortingAndSerching;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class section6_unit2 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++) arr[i] = sc.nextInt();
+
+        for(int i=0; i<n-1; i++){
+            for(int j=0; j<n-i-1; j++){
+                if(arr[j]>arr[j+1]){
+                    int tmp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = tmp;
+                }
+            }
+        }
+        for(int i=0; i<n; i++) System.out.print(arr[i]+ " ");
+
+    }
+}
