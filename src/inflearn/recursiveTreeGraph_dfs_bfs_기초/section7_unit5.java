f21d04a9e85f6d7575be793e038bc4ad0baaf0fa
김상익 <ksik518@naver.com>
2023-04-14 23:37:27 +0900

    section7_unit5_이진트리순회(DFS : Depth-First Search)

--- src/inflearn/recursiveTreeGraph_dfs_bfs_기초/section7_unit5.java
+++ src/inflearn/recursiveTreeGraph_dfs_bfs_기초/section7_unit5.java
@@ -3,21 +3,20 @@ import java.util.*;
 
 class Node{
     int data;
-    Node lt, rt;
-    public Node(int val) {
+    Node lt, rt; // 왼쪽과 오른쪽 자식 Node 주소 저장 변수
+    public Node(int val){
         data=val;
         lt=rt=null;
+        }
     }
-}
 
 public class section7_unit5{
     Node root;
     public void DFS(Node root){
-        if(root==null)
-            return;
-        else{
+        if(root==null) return; // 말단 노드
+        else{ // print 위치에 따라 달라짐
             DFS(root.lt);
-            System.out.print(root.data+" ");
+            System.out.print(root.data+" "); // 중위 순회
             DFS(root.rt);
         }
     }
