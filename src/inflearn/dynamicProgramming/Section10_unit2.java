305e8e0b093130f8423f577b732acb1d0bb3791d
김상익 <ksik518@naver.com>
2023-07-03 23:39:54 +0900

    Section10_unit2_돌다리 건너기

---
+++ src/inflearn/dynamicProgramming/Section10_unit2.java
@@ -0,0 +1,30 @@
+package inflearn.dynamicProgramming;
+
+import java.util.Scanner;
+
+public class Section10_unit2 {
+    static int[] dy;
+
+    public int solution (int n){
+
+        dy[1] = 1;
+        dy[2] = 2;
+
+        for (int i = 3; i <= n+1; i++) {
+            dy[i] = dy[i-2] + dy[i-1];
+        }
+
+        return dy[n+1];
+    }
+    public static void main(String[] args) {
+
+        Section10_unit2 T = new Section10_unit2();
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        dy = new int[n+2];
+
+        System.out.println(T.solution(n));
+    }
+
+}
