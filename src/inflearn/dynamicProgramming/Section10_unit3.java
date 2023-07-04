a2746541b353040c55373b9ef0dd076e30a24698
김상익 <ksik518@naver.com>
2023-07-04 17:15:46 +0900

    Section10_unit3_최대 부분 증가수열

---
+++ src/inflearn/dynamicProgramming/Section10_unit3.java
@@ -0,0 +1,34 @@
+package inflearn.dynamicProgramming;
+
+import java.util.*;
+
+public class Section10_unit3 {
+    static int[] dy;
+    static int solution(int[] arr){
+        int answer = 0;
+
+        dy = new int[arr.length];
+        dy[0]=1;
+        for (int i=1; i<arr.length; i++){
+            int max = 0;
+            for (int j=i; j>=0; j--){
+                if (arr[j] < arr[i] && dy[j] > max) max = dy[j];
+            }
+
+            dy[i] = max+1;
+            answer = Math.max(answer, dy[i]);
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt(); // 입력되는 데이터의 수
+        int[] arr = new int[n];
+
+        for (int i=0; i<n; i++) arr[i] = sc.nextInt();
+
+        System.out.println(solution(arr));
+    }
+}
